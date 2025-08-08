bc88d150ccc34541f84990642db0a1b49ca856e1
Pablo Omar Oviedo <pablooviedo58@gmail.com>
2025-08-08 09:20:36 -0300

    Reemplazo completo del proyecto con la última versión funcional

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,3 @@
-// Archivo: android/settings.gradle.kts
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
@@ -13,18 +12,15 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-
         gradlePluginPortal()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.0" apply false
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
-    // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "8.10.0" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
 include(":app")
